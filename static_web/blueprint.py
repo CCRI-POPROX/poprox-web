5545133bc9b3eabf03240a95344b804b7eaebc70
Daniel Kluver <kluve018@umn.edu>
2025-05-29 21:46:39 +0000

    convert static site to jinja2 rendered website

--- static_web/blueprint.py
+++ static_web/blueprint.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, send_from_directory
+import json
+
+import markdown
+from flask import Blueprint, render_template, send_from_directory
 
 static_web = Blueprint("static_web", __name__, template_folder="templates")
 
@@ -11,11 +14,6 @@ def public_img(filename):
 @static_web.route("/documents/<path:filename>")
 def public_documents(filename):
     return send_from_directory("static_web/public/documents", filename)
-
-
-@static_web.route("/data/<path:filename>")
-def public_data(filename):
-    return send_from_directory("static_web/public/data", filename)
 
 
 @static_web.route("/css/<path:filename>")
@@ -31,35 +29,45 @@ def public_assets(filename):
 @static_web.route("/about")
 @static_web.route("/about/")
 def about():
-    return send_from_directory("static_web/_site/about/", "index.html")
+    page_content = open("static_web/data/about.md").read()
+    content = markdown.markdown(page_content)
+    return render_template("markdown_page.html", content=content)
 
 
 @static_web.route("/contact")
 @static_web.route("/contact/")
 def contact():
-    return send_from_directory("static_web/_site/contact/", "index.html")
+    page_content = open("static_web/data/support.md").read()
+    content = markdown.markdown(page_content)
+    return render_template("markdown_page.html", content=content)
 
 
 @static_web.route("/experimenter")
 @static_web.route("/experimenter/")
 def experimenter():
-    return send_from_directory("static_web/_site/experimenter/", "index.html")
+    page_content = open("static_web/data/researcher.md").read()
+    content = markdown.markdown(page_content)
+    return render_template("markdown_page.html", content=content)
 
 
 @static_web.route("/newsletter")
 @static_web.route("/newsletter/")
 def newsletter():
-    return send_from_directory("static_web/_site/newsletter/", "index.html")
+    page_content = open("static_web/data/subscriber.md").read()
+    content = markdown.markdown(page_content)
+    return render_template("markdown_page.html", content=content)
 
 
 @static_web.route("/people")
 @static_web.route("/people/")
 def people():
-    return send_from_directory("static_web/_site/people/", "index.html")
+    team_info = open("static_web/data/teamInfo.json").read()
+    team_info = json.loads(team_info)
+    return render_template("people.html", teamInfo=team_info)
 
 
 @static_web.route("/")
 @static_web.route("/home")
 @static_web.route("/home/")
 def home():
-    return send_from_directory("static_web/_site/home/", "index.html")
+    return render_template("static_home.html")
