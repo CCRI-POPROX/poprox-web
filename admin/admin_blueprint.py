94fe5bacc5298814a653f94d6761c3ac1b567eb2
Daniel Kluver <kluve018@umn.edu>
2025-07-03 20:02:07 +0000

    finish basic team crud features

--- admin/admin_blueprint.py
+++ admin/admin_blueprint.py
@@ -1,8 +1,10 @@
 from os import environ as env
+from uuid import uuid4
 
 from flask import Blueprint, redirect, render_template, request, url_for
 from flask_httpauth import HTTPBasicAuth
 from poprox_storage.aws import DB_ENGINE
+from poprox_storage.concepts.experiment import Team
 from poprox_storage.repositories.accounts import DbAccountRepository
 from poprox_storage.repositories.teams import DbTeamRepository
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -31,14 +33,33 @@ def show():
         return render_template("admin_home.html", error=error, teams=teams)
 
 
-@admin.route("/team/<team_id>")
+@admin.get("/team/<team_id>")
 @admin_auth.login_required
-def edit_team(team_id):
+def team_details(team_id):
+    error = request.args.get("error")
+
     with DB_ENGINE.connect() as conn:
         team_repo = DbTeamRepository(conn)
         account_repo = DbAccountRepository(conn)
         team = team_repo.fetch_team_by_id(team_id)
         members = account_repo.fetch_accounts(team.members)
+        return render_template("admin_edit_team.html", team=team, error=error, members=members)
+
+
+@admin.post("/team/<team_id>")
+@admin_auth.login_required
+def edit_team_name(team_id):
+    new_name = request.form.get("team_name")
+    with DB_ENGINE.connect() as conn:
+        team_repo = DbTeamRepository(conn)
+        account_repo = DbAccountRepository(conn)
+
+        team = team_repo.fetch_team_by_id(team_id)
+        team.team_name = new_name
+        team_repo.store_team(team)
+        members = account_repo.fetch_accounts(team.members)
+
+        conn.commit()
         return render_template("admin_edit_team.html", team=team, members=members)
 
 
@@ -54,6 +75,18 @@ def add_to_team(team_id):
             return redirect(
                 url_for("admin.edit_team", team_id=team_id, error=f"No account for email: '{email}' found ")
             )
-        team_repo._insert_team_membership(team_id, account.account_id)
+        team_repo.insert_team_membership(team_id, account.account_id)
         conn.commit()
         return redirect(url_for("admin.edit_team", team_id=team_id))
+
+
+@admin.post("/team")
+@admin_auth.login_required
+def new_team():
+    team_name = request.form.get("team_name", "untiteled team")
+    with DB_ENGINE.connect() as conn:
+        team_repo = DbTeamRepository(conn)
+        team = Team(team_id=uuid4(), team_name=team_name, members=[])
+        team_repo.store_team(team)
+        conn.commit()
+        return redirect(url_for("admin.edit_team", team_id=team.team_id))
